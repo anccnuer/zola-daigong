8687599b8f97f279219c2536e4b87acf5efadebd
Dazun <92565045+anccnuer@users.noreply.github.com>
2025-04-07 21:55:55 +0800

    init

---
+++ static/main.js
@@ -0,0 +1,17 @@
+function switchon() {
+    const menu = document.getElementById('menu');
+    const on = document.getElementById('switch-on');
+    menu.style.display = 'block';
+    on.style.display = 'none';
+}
+
+function switchoff() {
+    const menu = document.getElementById('menu');
+    const on = document.getElementById('switch-on');
+    menu.style.display = 'none';
+    on.style.display = 'block';
+}
+
+function gotop() {
+    
+}
