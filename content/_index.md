b5613d7586dbb75c06109a480420293ef4303912
anccnuer <x.rong@yandex.com>
2025-12-21 16:45:08 +0800

    Add pagination feature to blog

--- content/_index.md
+++ content/_index.md
@@ -2,4 +2,5 @@
 sort_by="date"
 template = "index.html"
 page_template = "page.html"
+paginate_by = 5
 +++
--- fontsub.bat
+++
@@ -1,4 +0,0 @@
-char.exe
-hb-subset --output-file=./static/assets/Zhuque.ttf input.ttf --text-file=hanzi.txt --name-IDs=* --layout-features=* --no-hinting --drop-tables=DSIG,hdmx,VDMX,LTSH,PCLT,vhea,vmtx,VORG,gasp
-fontconv ./static/assets/Zhuque.ttf ./static/assets/Zhuque.woff2
-pause
